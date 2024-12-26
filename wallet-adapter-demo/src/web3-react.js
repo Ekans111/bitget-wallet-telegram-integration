07c5fbf251967574419080d18fb1b021fd25d10e
ekans111 <kobayasitorao111@gmail.com>
2024-12-26 17:01:46 +0900

    feat: omni-connect aptos wallet
    Co-authored-by: hiroyama122 <hiroyama920122@gmail.com>
    
    Co-authored-by: bluesky613 <shunjishima624@gmail.com>

--- wallet-adapter-demo/src/web3-react.js
+++
@@ -1,7 +0,0 @@
-export default function Web3ReactDemo() {
-  return (
-    <>
-      <a href="https://github.com/BitgetWalletTeam/web3-react/tree/BitgetWallet/example">https://github.com/BitgetWalletTeam/web3-react/tree/BitgetWallet/example</a>
-    </>
-  )
-};
